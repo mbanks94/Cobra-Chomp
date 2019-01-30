e0c8d702f5af9401f4816b13448bab0226077f27
Michael Banks <michaelbanks@Michaels-MacBook-Pro-2.local>
2019-01-29 21:12:35 -0600

    Added form post

--- server/server.js
+++ server/server.js
@@ -3,8 +3,12 @@ const path = require('path');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 
+
 const app = express();
-const dataPath = path.join(__dirname, "../data.json");
+const dataPath = path.join(__dirname, "../scores.json");
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 //Logs any req to the url
 app.use((req, res, next) => {
@@ -12,9 +16,17 @@ app.use((req, res, next) => {
     next();
 });
 
+app.post('/scores', (req, res) => {
+    let name = req.body.name;
+    let score = req.body.score;
+    res.send('Name: ' + name + ' Score: ' + score);
+    let writeData = fs.writeFileSync(dataPath, 
+        JSON.stringify('Name: ' + name + ' Score: ' + score),
+        'utf-8', err => console.log(err));
+});
+
 //Joins html, css, js files
 app.use(express.static(path.join(__dirname, '../public')));
-
 
 app.listen(3000, (res) => {
     console.log('Server is up on port 3000');

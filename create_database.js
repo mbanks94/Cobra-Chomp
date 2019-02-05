2a95e17accc753a785bd34d19374fd9e5fd900d7
Student <student@Admins-iMac.local>
2019-02-05 16:05:46 -0600

    Feb 5

--- create_database.js
+++ create_database.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('cobrachomp.db');
 
 db.serialize(() => {
-    db.run("CREATE TABLE users (name TEXT, score INT)");
+    db.run("CREATE TABLE users (id INTEGER Primary Key AUTOINCREMENT, name TEXT, score INT)");
     console.log('Success');
 });
 

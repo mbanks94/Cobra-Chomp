dbc6d07eb85a48c391c7961f5a337e1b38420216
Michael Banks <michaelbanks@Michaels-MacBook-Pro-2.local>
2019-02-11 08:10:38 -0600

    Working on new responsive landing page

--- create_database.js
+++
@@ -1,9 +0,0 @@
-const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database('cobrachomp.db');
-
-db.serialize(() => {
-    db.run("CREATE TABLE users (id INTEGER Primary Key AUTOINCREMENT, name TEXT, score INT)");
-    console.log('Success');
-});
-
-db.close();
--- server/server.js
+++ server/server.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database('cobrachomp.db');
+const dbPath = path.join(__dirname, '../db/cobrachomp.db');
 
-
-
+app.use(express.json());
 //Joins html, css, js files
 app.use(express.static(path.join(__dirname, '../public')));
-app.use(bodyParser.urlencoded({extended:false}));
 //Logs any req to the url
 app.use((req, res, next) => {
     console.log(req.url);
     next();
+});
+
+const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, 
+    (err) => {
+        if (err) {
+            throw err;
+        } else {
+            console.log('Connected to DB');
+        }
 });
 
 app.get('/users', (req, res) => {
@@ -24,18 +30,7 @@ app.get('/users', (req, res) => {
 });
 
 app.post('/scores', (req, res) => {
-    // let name = req.body.name;
-    // let score = req.body.score;
-    // let item = {"name":name, "score":score};
-    // fs.appendFileSync(dataPath, 
-    //     JSON.stringify(item, null, 4), 'utf-8', err => console.log(err));
-    
-    db.run(
-        "INSERT INTO users VALUES ($name, $score)", 
-        {
-            $name: req.body.name,
-            $score: req.body.score
-        },
+    db.run("INSERT INTO users VALUES ($name, $score)", {$name: req.body.name, $score: req.body.score},
         (err) => {
             if (err) {
                 res.send({message: 'error in app.post(/scores)'});
